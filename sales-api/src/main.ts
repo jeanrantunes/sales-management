e0f9b432df5d61949b8c3f8b8292cd72d0de6d01
Jean Roberto Antunes <jean.antunes@MacBook-Air.local>
2023-04-18 22:49:30 -0300

    feat: 🎸 config docker to work on nest and mysql

--- sales-api/src/main.ts
+++ sales-api/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.NESTJS_APP_DOCKER_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  await app.listen(port).then(() => {
+    console.log(`Running at http://localhost:${port}`);
+  });
 }
+
 bootstrap();
